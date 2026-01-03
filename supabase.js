f5fb3152e323586077d396b8a762c6b011b55d4e
AbiAnton <gale112358@gmail.com>
2026-01-03 01:46:50 -0600

    Remove unused supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = 'https://gmwspbobwxflqrvkyqmj.supabase.co';
-const supabaseKey = 'sb_publishable_S6DdY0PXY4krL8mPrqtgsA_eCFWgBi0';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
